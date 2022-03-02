535f7da517f34081250eb4ed2feb784ff2c54e98
cooksies <lacadinjohn@gmail.com>
2022-03-01 20:01:58 -0500

    Finally complete
    can be submitted or have additional items placed in

--- midterm_217261066/eventHandler.js
+++ midterm_217261066/eventHandler.js
@@ -1,2 +1,4 @@
 var phoneNode = document.getElementById("number");
 phoneNode.addEventListener("change", chkPhone, false);
+var emailNode = document.getElementById("email");
+emailNode.addEventListener("change", chkEmail, false);
--- midterm_217261066/myScript.js
+++ midterm_217261066/myScript.js
@@ -7,7 +7,21 @@ function chkPhone(event) {
       if (pos != 0) {
         alert("The phone number you entered (" + myPhone.value +
               ") is not in the correct form. \n" +
-              "The correct form is: ddd-ddd-dddd \n" +
+              "The correct form is: 123-456-7890 \n" +
               "Please go back and fix your phone number");
+      } 
+    }
+
+// The event handler function for the phone number text box
+function chkEmail(event) {
+    // Get the target node of the event
+      var myEmail = event.currentTarget;
+    // Test the format of the input phone number
+      var pos = myEmail.value.search(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+      if (pos != 0) {
+        alert("The email you entered (" + myEmail.value +
+              ") is not in the correct form. \n" +
+              "The correct form is: username@domain.com \n" +
+              "Please go back and fix your email");
       } 
     }
